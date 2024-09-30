af919fd3667e5845876280e0c1934799307cd6b2
Md Mustaqim Alam <mustaqimalam85@gmail.com>
2024-09-30 12:26:14 +0530

    Todo

--- src/App.jsx
+++ src/App.jsx
@@ -51,7 +51,7 @@ function App() {
       <div className="bg-[#120842] min-h h- py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todos
+           Connex Assignment - Todo website
           </h1>
           <div className="mb-4">
             <TodoForm />
